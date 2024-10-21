2b93df8868a25a48bf75d2d6a3b672446783a545
coolguy46 <104799027+coolguy46@users.noreply.github.com>
2024-10-21 16:20:12 -0700

    Add files via upload

---
+++ lib/firebaseAdmin.ts
@@ -0,0 +1,20 @@
+// lib/firebaseAdmin.ts
+import admin from 'firebase-admin';
+
+// Initialize Firebase Admin SDK if not already initialized
+if (!admin.apps.length) {
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const auth = admin.auth();
+const db = admin.firestore();
+
+export { auth, db };
---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage"; 
+import { doc, setDoc } from "firebase/firestore";
+
+// Your Firebase configuration object
+const firebaseConfig = {
+  apiKey: 'AIzaSyDRNdF2bfIMLL11egHLvb-fOeNZrUFDi80',
+  authDomain: 'nutrivision-37b5f.firebaseapp.com',
+  projectId: 'nutrivision-37b5f',
+  storageBucket: 'nutrivision-37b5f.appspot.com',
+  messagingSenderId: '94959546917',
+  appId: '1:94959546917:web:a8fa48e337e371d8f545cc',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and Google Auth Provider
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app)
+const storage = getStorage(app); 
+
+
+export { auth, googleProvider, db, storage};
