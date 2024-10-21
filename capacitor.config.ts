6995bc1c5c74d269bc2b768d27dca95f5584ee5d
coolguy46 <104799027+coolguy46@users.noreply.github.com>
2024-10-21 16:14:46 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nutrivision.app',
+  appName: 'NutriVision',
+  webDir: 'public'
+};
+
+export default config;
---
+++ tailwind.config.ts
@@ -0,0 +1,114 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    darkMode: ['class'],
+    content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // Include this if using the new app directory structure
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+			darkMode: 'class',
+  			primary: {
+  				'100': '#3498db',
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				'100': '#f1c40f',
+  				dark: '#ff9900',
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			text: '#333333',
+  			green: {
+  				'100': '#2ecc71',
+  				light: '#34c759'
+  			},
+  			red: {
+  				'100': '#e74c3c',
+  				'600': '#e53935'
+  			},
+  			gray: {
+  				'100': '#f3f4f6',
+  				'600': '#4b5563',
+  				'700': '#374151',
+  				'800': '#1f2937'
+  			},
+  			purple: {
+  				'500': '#8b5cf6'
+  			},
+  			indigo: {
+  				'700': '#4f46e5'
+  			},
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			'3xl': '1.5rem',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		boxShadow: {
+  			'2xl': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+		
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
