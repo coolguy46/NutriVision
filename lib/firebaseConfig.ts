8877db05f51a8173bba731f160e01abb69dc0699
coolguy46 <104799027+coolguy46@users.noreply.github.com>
2024-10-23 23:05:15 -0700

    Update firebaseConfig.ts

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -6,12 +6,12 @@ import { doc, setDoc } from "firebase/firestore";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: 'AIzaSyDRNdF2bfIMLL11egHLvb-fOeNZrUFDi80',
-  authDomain: 'nutrivision-37b5f.firebaseapp.com',
-  projectId: 'nutrivision-37b5f',
-  storageBucket: 'nutrivision-37b5f.appspot.com',
-  messagingSenderId: '94959546917',
-  appId: '1:94959546917:web:a8fa48e337e371d8f545cc',
+  apiKey: process.env.GOOGLE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
