38dbeb87993e678f0f740f1f507cb47b4c434db2
coolguy46 <104799027+coolguy46@users.noreply.github.com>
2024-10-24 18:29:01 -0700

    Update firebaseConfig.ts

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -6,7 +6,7 @@ import { doc, setDoc } from "firebase/firestore";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: process.env.GOOGLE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
