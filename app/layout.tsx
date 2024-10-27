046c6c4a6b0f6eac5db01aaf3c727eb78743b508
coolguy46 <104799027+coolguy46@users.noreply.github.com>
2024-10-26 17:27:13 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import { AnalysisProvider } from '@/context/AnalysisContext';
 import Layout from '@/components/layout1';
-
+import { usePathname } from 'next/navigation';
 // Remove "use client" as this should be a server component
 
 export default function RootLayout({
@@ -8,6 +10,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname();
+  const noLayoutRoutes = ['/signin', '/signup', '/check-email', '/verify-email'];
+
+  // Check if the current route matches any of the excluded routes
+  if (noLayoutRoutes.includes(pathname)) {
+    return (
+      <html lang="en">
+      <body>
+        {children}
+        </body>
+    </html>
+    )
+  }
   return (
     <html lang="en">
       <body>
